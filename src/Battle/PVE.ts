c0935b351b44725b807ca5fc273cfd7e5491f330
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-05-30 12:25:20 -0300

    feat: Requisito 11 - Criar a classe PVE

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,19 @@
+import Fighter, { SimpleFighter } from '../Fighter';
+import Battle from './Battle';
+
+export default class PVE extends Battle {
+  private _monsters: (SimpleFighter | Fighter)[];
+
+  constructor(player: Fighter, monsters: (SimpleFighter | Fighter)[]) {
+    super(player);
+    this._monsters = monsters;
+  }
+
+  fight(): number {
+    this._monsters.forEach((monster) => {
+      this.player.attack(monster);
+      monster.attack(this.player);
+    });
+    return super.fight();
+  }
+}
--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -2,18 +2,16 @@ import Fighter from '../Fighter';
 import Battle from './Battle';
 
 export default class PVP extends Battle {
-  private _player1: Fighter;
   private _player2: Fighter;
 
   constructor(player1: Fighter, player2: Fighter) {
     super(player1);
-    this._player1 = player1;
     this._player2 = player2;
   }
 
   fight(): number {
-    this._player1.attack(this._player2);
-    this._player2.attack(this._player1);
-    return this._player1.lifePoints === -1 ? -1 : 1;
+    this.player.attack(this._player2);
+    this._player2.attack(this.player);
+    return super.fight();
   }
 }
